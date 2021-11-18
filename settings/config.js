b09dfe64598b204425390af13a83b4892153d146
FaluJL <94586360+FaluJL@users.noreply.github.com>
2021-11-17 21:38:06 -0600

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Shan',
+        botName: 'JumiBot4.0',
+        ownerName: 'User1632',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
